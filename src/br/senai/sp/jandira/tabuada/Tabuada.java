d6a5e56095d5b9095e3787c13e9af35d2baaa6cb
ALICE CAMPOS DA SILVA <25203690@cfp127aluno.local>
2025-11-03 17:01:14 -0300

    Enviando

---
+++ src/br/senai/sp/jandira/tabuada/Tabuada.java
@@ -0,0 +1,60 @@
+package br.senai.sp.jandira.tabuada;
+
+import java.util.Scanner;
+
+public class Tabuada {
+
+    public int multiplicando;
+    public int multiplicadorInicial;
+    public int multiplicadorFinal;
+    public String[] tabuada;
+
+    public void coletardados(){
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Qual é o valor do multiplicando? ");
+        multiplicando = leitor.nextInt();
+
+        System.out.println("Qual o valor do multiplicador inicial? ");
+        multiplicadorInicial = leitor.nextInt();
+
+        System.out.println("Qual o valor do multiplicador final? ");
+        multiplicadorFinal = leitor.nextInt();
+
+
+    }
+
+    public void calcularTabuada(){
+        int apoio = 0;
+
+        if (multiplicadorFinal < multiplicadorInicial){
+            apoio = multiplicadorFinal;
+            multiplicadorFinal = multiplicadorInicial;
+            multiplicadorInicial = apoio;
+        }
+
+        int tamanho = multiplicadorFinal -  multiplicadorInicial + 1;
+        tabuada = new String[tamanho];
+
+        int i =0;
+        while (i < tamanho){
+            int produto = multiplicando * multiplicadorInicial;
+            tabuada[i] = multiplicando + " X " + multiplicadorInicial + " = "  + produto;
+            multiplicadorInicial = multiplicadorInicial + 1;
+            i = i + 1;
+        }
+
+        exibirTabuada();
+
+    }
+
+    public void exibirTabuada(){
+        System.out.println("Resultado da sua tabuada:");
+
+        int i = 0;
+        while (i < tabuada.length){
+            System.out.println(tabuada[i]);
+            i++; // i =mi + 1
+        }
+    }
+}
---
+++ src/br/senai/sp/jandira/tabuada/TabuadaMain.java
@@ -0,0 +1,12 @@
+package br.senai.sp.jandira.tabuada;
+
+public class TabuadaMain {
+    public static void main(String[] args) {
+        System.out.println("Tabuada main");
+        Tabuada tabuada = new Tabuada();
+        tabuada.coletardados();
+        tabuada.calcularTabuada();
+
+
+    }
+}
